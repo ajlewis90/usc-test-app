8ab4aa8fb0be150df94dbb78adadbe7415733119
Arthur Joel Lewis <ajlewis90@users.noreply.github.com>
2025-07-25 22:31:01 +0000

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ['28876bc9-b99a-4f62-8e8d-72b648ac2825-00-13c724o05xof8.picard.replit.dev', '.picard.replit.dev']
-})
+  allowedHosts: [
+    "28876bc9-b99a-4f62-8e8d-72b648ac2825-00-13c724o05xof8.picard.replit.dev",
+    ".picard.replit.dev",
+  ],
+});
